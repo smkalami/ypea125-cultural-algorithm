8602d7e56443bfa6b64a8f3d0032f850b83871af
Mostapha Kalami <sm.kalami@gmail.com>
2020-12-11 23:22:47 +0330

    first commit

---
+++ Cultural Algorithm/main.m
@@ -0,0 +1,17 @@
+%
+% Copyright (c) 2015, Mostapha Kalami Heris & Yarpiz (www.yarpiz.com)
+% All rights reserved. Please read the "LICENSE" file for license terms.
+%
+% Project Code: YPEA125
+% Project Title: Implementation of Cultural Algorithm in MATLAB
+% Publisher: Yarpiz (www.yarpiz.com)
+% 
+% Developer: Mostapha Kalami Heris (Member of Yarpiz Team)
+% 
+% Cite as:
+% Mostapha Kalami Heris, Cultural Algorithm (CA) in MATLAB (URL: https://yarpiz.com/425/ypea125-cultural-algorithm), Yarpiz, 2015.
+% 
+% Contact Info: sm.kalami@gmail.com, info@yarpiz.com
+%
+
+ca;
---
+++ README.md
@@ -0,0 +1,15 @@
+# Cultural Algorithm (CA) in MATLAB
+
+<p align="center">
+    <img src="https://yarpiz.com/wp-content/uploads/2015/09/ypea125-cultural-algorithm.jpg" alt="Cultural Algorithm (CA) in MATLAB">
+</p>
+
+This is an implementation of Cultural Algorithm (CA) in MATLAB.
+
+For more information, visit following URL:
+https://yarpiz.com/425/ypea125-cultural-algorithm
+
+## Citing This Work
+You can cite this code as follows:
+
+**Mostapha Kalami Heris, Cultural Algorithm (CA) in MATLAB (URL: https://yarpiz.com/425/ypea125-cultural-algorithm), Yarpiz, 2015.**
